1d5f7ca6fd358affd667d6c2ed7fc59944f6b1dc
som@3000 <guptanathsom@gmail.com>
2026-06-04 12:51:02 +0530

    adds changes to github

--- src/main/java/org/learning/todo/filter/LoggingFilter.java
+++ src/main/java/org/learning/todo/filter/LoggingFilter.java
@@ -5,41 +5,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-<<<<<<< HEAD
 import org.springframework.http.server.ServerHttpRequest;
-=======
->>>>>>> refs/remotes/origin/main
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 public class LoggingFilter implements Filter {
-<<<<<<< HEAD
 
   private final Logger logger = LoggerFactory.getLogger("RequestLogger");
-
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-    HttpServletRequest httpRequest = (HttpServletRequest) request;
-    logger.info("{} {}", httpRequest.getMethod(), httpRequest.getRequestURI());
-    long startTime = System.currentTimeMillis();
-    try {
-      chain.doFilter(request, response);
-    }
-    finally {
-      long timeTaken = System.currentTimeMillis() - startTime;
-      HttpServletResponse httpResponse = (HttpServletResponse) response;
-      logger.info("{} {} Status: {} Time(ms) {}",
-              httpRequest.getMethod(),
-              httpRequest.getRequestURI(),
-              httpResponse.getStatus(),
-              timeTaken);
-    }
-  }
-=======
-    private final Logger logger = LoggerFactory.getLogger("RequestLogger");
-
     @Override
     public void doFilter(ServletRequest request,
                          ServletResponse response,
@@ -62,5 +36,4 @@ public class LoggingFilter implements Filter {
             );
         }
     }
->>>>>>> refs/remotes/origin/main
 }
